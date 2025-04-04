9c16a532e1b06c5b7884af0a30e2b8b8718d7d08
Ruslan Muliarchuk <rmularcuk9@gmail.com>
2025-04-04 14:03:07 +0300

    updated form registry

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -7,11 +7,12 @@ import { validateFormFun } from "@/store/validateFormFunction";
 import { useModalStore } from "@/store/ModalWindowState";
 import Button1 from "@/components/button1";
 import { signIn } from "next-auth/react";
+import { kurale } from "@/lib/fonts";
 
 import '../../styles/formRegistry.css'
 
 export default function Registration() {
-    const {registry} = useAuth();
+    const {registry, user} = useAuth();
     const {openModal, closeModal} = useModalStore();
     const {validateName, validateEmail, validatePassword} = validateFormFun();
     const { formData, handleChange, resetForm, errors, validateFormReg } = useForm(
@@ -38,18 +39,26 @@ export default function Registration() {
         openModal({title: result.message, buttonText: 'Продовжити', onConfirm: closeModal})
         console.log(result);
     }
+    if (user) {
+      return <div className="user-registry-container">
+        <h1 className={`user-registry-title ${kurale.className}`}>Ви вже авторизовані!</h1>
+        <Button1 title="Перейти до профіля" clas="button-global" href={`/profile/${user.id}`} />
+        <Button1 title="Повернутися назад" clas="button-global" onClick={()=>window.history.back()} />
+      </div>
+    }
 
     return (
-        <div className="form-registry-container">
+        <div className={`form-registry-container ${kurale.className}`}>
+          <h1>Реєстрація</h1>
          <form className="container-log-in" onSubmit={handleSubmit}>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Ім'я" required />
+            <input className="form-reg-input" type="text" name="name" value={formData.name} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Ім'я" required />
             <p className="form-reg-error">{errors.name}</p>
-           <input type="email" name="email" value={formData.email} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Email" required />
+           <input className="form-reg-input" type="email" name="email" value={formData.email} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Email" required />
            <p className="form-reg-error">{errors.email}</p>
-           <input type="password" name="password" value={formData.password} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Пароль" required />
+           <input className="form-reg-input" type="password" name="password" value={formData.password} onChange={handleChange} onBlur={(e)=> validateFormReg(e.target.name)} placeholder="Пароль" required />
            <p className="form-reg-error">{errors.password}</p>
-           <div className="container-input-radio">
            <label>Стать:</label>
+           <div className="container-input-radio">
             <input
                     type="radio"
                     name="sex"
@@ -68,8 +77,9 @@ export default function Registration() {
 
            </div>
            <button className="button-global" type="submit">Зареєструватися</button>
-           <Button1 title="Увійти через Google" clas="button-global" onClick={()=>signIn('google')} />
         </form>
+        <h3>Або Увійдіть за допомогою:</h3>
+        <Button1 title="Увійти через Google" clas="button-global" onClick={()=>signIn('google')} />
         </div>
     )
 }
